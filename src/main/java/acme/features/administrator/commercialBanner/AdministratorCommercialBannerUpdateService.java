dd3b1d054224986e566d2ac0ef07ee19be1afb8c
Alvaro <Alvaro@DESKTOP-AMLPO6E>
2019-11-23 15:50:28 +0100

    Uploading the starter

---
+++ src/main/java/acme/features/administrator/commercialBanner/AdministratorCommercialBannerUpdateService.java
@@ -0,0 +1,95 @@
+
+package acme.features.administrator.commercialBanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.banners.CommercialBanner;
+import acme.framework.components.Errors;
+import acme.framework.components.Model;
+import acme.framework.components.Request;
+import acme.framework.entities.Administrator;
+import acme.framework.services.AbstractUpdateService;
+
+@Service
+public class AdministratorCommercialBannerUpdateService implements AbstractUpdateService<Administrator, CommercialBanner> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AdministratorCommercialBannerRepository repository;
+
+
+	// AbstractShowService<Authenticated, Announcement> interface -------------
+
+	@Override
+	public boolean authorise(final Request<CommercialBanner> request) {
+		assert request != null;
+
+		return true;
+	}
+
+	@Override
+	public void bind(final Request<CommercialBanner> request, final CommercialBanner entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+		request.bind(entity, errors);
+
+	}
+
+	@Override
+	public void unbind(final Request<CommercialBanner> request, final CommercialBanner entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "slogan", "targetURL", "creditCard", "picture");
+	}
+
+	@Override
+	public CommercialBanner findOne(final Request<CommercialBanner> request) {
+		assert request != null;
+
+		CommercialBanner result;
+		int id;
+
+		id = request.getModel().getInteger("id");
+		result = this.repository.findOneById(id);
+
+		return result;
+	}
+
+	@Override
+	public void validate(final Request<CommercialBanner> request, final CommercialBanner entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+		String regexpurl = "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)";
+		String regexpcc = "^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})$";
+
+		if (!errors.hasErrors("slogan")) {
+			errors.state(request, !entity.getSlogan().isEmpty(), "slogan", "javax.validation.constraints.NotBlank.message");
+		}
+		if (!errors.hasErrors("targetURL")) {
+			errors.state(request, !entity.getTargetURL().matches(regexpurl), "targetURL", "javax.validation.constraints.Pattern.message", regexpurl);
+		}
+		if (!errors.hasErrors("picture")) {
+			errors.state(request, !entity.getTargetURL().matches(regexpurl), "picture", "javax.validation.constraints.Pattern.message", regexpurl);
+		}
+		if (!errors.hasErrors("creditCard")) {
+			errors.state(request, !entity.getTargetURL().matches(regexpcc), "creditCard", "javax.validation.constraints.Pattern.message", regexpcc);
+		}
+
+	}
+
+	@Override
+	public void update(final Request<CommercialBanner> request, final CommercialBanner entity) {
+		assert request != null;
+		assert entity != null;
+
+		this.repository.save(entity);
+	}
+
+}
---
+++ src/main/population/create-schema.sql
@@ -0,0 +1,263 @@
+
+    create table `administrator` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `alferez_bulletin` (
+       `id` integer not null,
+        `version` integer not null,
+        `author` varchar(255),
+        `moment` datetime(6),
+        `type` varchar(255),
+        `url` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `announcement` (
+       `id` integer not null,
+        `version` integer not null,
+        `moment` datetime(6),
+        `more_info` varchar(255),
+        `text` varchar(255),
+        `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `anonymous` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `authenticated` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `cardenas_bulletin` (
+       `id` integer not null,
+        `version` integer not null,
+        `author` varchar(255),
+        `book` varchar(255),
+        `genre` varchar(255),
+        `isbn` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `challenge` (
+       `id` integer not null,
+        `version` integer not null,
+        `deadline` datetime(6),
+        `description` varchar(255),
+        `goal` varchar(255),
+        `reward` varchar(255),
+        `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `chellik_bulletin` (
+       `id` integer not null,
+        `version` integer not null,
+        `apellido` varchar(255),
+        `fecha` datetime(6),
+        `nombre` varchar(255),
+        `role` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `commercial_banner` (
+       `id` integer not null,
+        `version` integer not null,
+        `picture` varchar(255),
+        `slogan` varchar(255),
+        `targeturl` varchar(255),
+        `credit_card` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `company_record` (
+       `id` integer not null,
+        `version` integer not null,
+        `ceoname` varchar(255),
+        `activities_description` varchar(255),
+        `company_name` varchar(255),
+        `contact_email` varchar(255),
+        `contact_phone` varchar(255),
+        `incorporated` bit,
+        `sector` varchar(255),
+        `star_score` double precision,
+        `website` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `consumer` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        `company` varchar(255),
+        `sector` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `customisation_parameter` (
+       `id` integer not null,
+        `version` integer not null,
+        `spam_threshold` double precision,
+        `spam_words_en` varchar(255),
+        `spam_words_es` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `escobar_bulletin` (
+       `id` integer not null,
+        `version` integer not null,
+        `author` varchar(255),
+        `birthdate` datetime(6),
+        `level` varchar(255),
+        `moment` datetime(6),
+        `text` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `investor_record` (
+       `id` integer not null,
+        `version` integer not null,
+        `investing_statement` varchar(255),
+        `investor_name` varchar(255),
+        `sector` varchar(255),
+        `stars` double precision,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `non_commercial_banner` (
+       `id` integer not null,
+        `version` integer not null,
+        `picture` varchar(255),
+        `slogan` varchar(255),
+        `targeturl` varchar(255),
+        `jingle` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `offer` (
+       `id` integer not null,
+        `version` integer not null,
+        `creation` datetime(6),
+        `deadline` datetime(6),
+        `reward_max_amount` double precision,
+        `reward_max_currency` varchar(255),
+        `reward_min_amount` double precision,
+        `reward_min_currency` varchar(255),
+        `text` varchar(255),
+        `ticker` varchar(255),
+        `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `provider` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        `company` varchar(255),
+        `sector` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `requests` (
+       `id` integer not null,
+        `version` integer not null,
+        `creation` datetime(6),
+        `deadline` datetime(6),
+        `reward_amount` double precision,
+        `reward_currency` varchar(255),
+        `text` varchar(255),
+        `ticker` varchar(255),
+        `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `rosado_bulletin` (
+       `id` integer not null,
+        `version` integer not null,
+        `image` varchar(255),
+        `name` varchar(255),
+        `video` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `shout` (
+       `id` integer not null,
+        `version` integer not null,
+        `author` varchar(255),
+        `moment` datetime(6),
+        `text` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `user_account` (
+       `id` integer not null,
+        `version` integer not null,
+        `enabled` bit not null,
+        `identity_email` varchar(255),
+        `identity_name` varchar(255),
+        `identity_surname` varchar(255),
+        `password` varchar(255),
+        `username` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `hibernate_sequence` (
+       `next_val` bigint
+    ) engine=InnoDB;
+
+    insert into `hibernate_sequence` values ( 1 );
+create index IDXnr284tes3x8hnd3h716tmb3fr on `challenge` (`deadline`);
+create index IDXmgvhy7dlsdj1h0c8yb46i6by1 on `challenge` (`title`, `deadline`);
+create index IDXh7bve39okm3gjs4blcchj12j2 on `company_record` (`star_score`);
+create index IDXk2t3uthe649ao1jllcuks0gv4 on `investor_record` (`stars`);
+create index IDX6vvdfk8bhs718y3psyx1p1p9d on `offer` (`id`, `deadline`);
+create index IDX2jcikaiqhqcdfy62sn4d3ph58 on `offer` (`ticker`, `deadline`);
+create index IDXq2o9psuqfuqmq59f0sq57x9uf on `offer` (`deadline`);
+
+    alter table `offer` 
+       add constraint UK_iex7e8fs0fh89yxpcnm1orjkm unique (`ticker`);
+create index IDX289opkba5wrr0u79x133vuui5 on `requests` (`id`, `deadline`);
+create index IDXolwqknfiuvtep9yrmfnwlnrs4 on `requests` (`ticker`, `deadline`);
+create index IDXmly5kwrpgadjkxv5t5dgw36hr on `requests` (`deadline`);
+
+    alter table `requests` 
+       add constraint UK_5v1h0kdr8vcps4i9e55k5gnc8 unique (`ticker`);
+
+    alter table `user_account` 
+       add constraint UK_castjbvpeeus0r8lbpehiu0e4 unique (`username`);
+
+    alter table `administrator` 
+       add constraint FK_2a5vcjo3stlfcwadosjfq49l1 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `anonymous` 
+       add constraint FK_6lnbc6fo3om54vugoh8icg78m 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `authenticated` 
+       add constraint FK_h52w0f3wjoi68b63wv9vwon57 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `consumer` 
+       add constraint FK_6cyha9f1wpj0dpbxrrjddrqed 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `provider` 
+       add constraint FK_b1gwnjqm6ggy9yuiqm0o4rlmd 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
